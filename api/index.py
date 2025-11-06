12ef6d38b3dac735deb3572b1f63cdbd909fdb1d
legando404 <legando404@gmail.com>
2025-11-06 10:45:43 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -92,38 +92,39 @@ async def post_order(client, first_name, last_name, email, subject, text, html, 
 async def get_mail(username, password, imap_server):
     array = []
     print('connecting to imap server...')
-    
     with MailBox(imap_server).login(username, password, initial_folder='Novers Казань') as mailbox:
-        print('fetching unread emails...')
-        
+        print('fetching...')
         exists = mailbox.folder.exists('Novers Казань/INBOX|Казань')
         if not exists:
             mailbox.folder.create('Novers Казань/INBOX|Казань')
-        
-        # Только непрочитанные письма
-        for msg in mailbox.fetch(AND(seen=False)):
-            attachments = [a for a in msg.attachments]
-
-            # создаем заказ и пр.
-            # ...
+       
+        # Берём все письма, вне зависимости от прочитанности
+        for msg in mailbox.fetch():
+            mailbox.move(msg.uid, 'Novers Казань/INBOX|Казань') 
             
-            # пометить как прочитанное, чтобы не подхватывалось в будущем
-            mailbox.flag(msg.uid, ['\\Seen'], True)
+            attachments = []
+            for a in msg.attachments:
+                print(a.filename)
+                attachments.append(a)
             
-            # перемещаем в архив
-            mailbox.move(msg.uid, 'Novers Казань/INBOX|Казань')
+            name = re.search('(.*) <' + msg.from_ + '>', msg.from_values.full).group(1).split(' ')
+            lastName = name[-1]
+            name.pop(-1)
+            firstName = ' '.join(name)
             
-            array.append({
+            data = {
                 "email": msg.from_,
-                "first_name": "",
-                "last_name": "",
+                "first_name": firstName,
+                "last_name": lastName,
                 "subject": msg.subject,
                 "text": msg.text,
                 "html": msg.html,
                 "attachments": attachments
-            })
-    
-    return array
+            }
+            print(data["email"])
+            print(msg.date, msg.from_, msg.subject, msg.from_values, name, len(msg.text or msg.html))
+            array.append(data)
+        return array
 
 
 async def task():
